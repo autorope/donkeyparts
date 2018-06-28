45b8ec47da1aff7c05bda43bbf2633e1a49b6264
Will Roscoe <wroscoe@gmail.com>
2018-06-27 22:41:43 -0700

    first commit

---
+++ donkey_parts_library/actuator.py
@@ -0,0 +1,182 @@
+
+class Maestro:
+    """
+    Pololu Maestro Servo controller
+    Use the MaestroControlCenter to set the speed & acceleration values to 0!
+    """
+    import threading
+
+    maestro_device = None
+    astar_device = None
+    maestro_lock = threading.Lock()
+    astar_lock = threading.Lock()
+
+    def __init__(self, channel, frequency = 60):
+        import serial
+
+        if Maestro.maestro_device == None:
+            Maestro.maestro_device = serial.Serial('/dev/ttyACM0', 115200)
+
+        self.channel = channel
+        self.frequency = frequency
+        self.lturn = False
+        self.rturn = False
+        self.headlights = False
+        self.brakelights = False
+
+        if Maestro.astar_device == None:
+            Maestro.astar_device = serial.Serial('/dev/ttyACM2', 115200, timeout= 0.01)
+
+    def set_pulse(self, pulse):
+        # Recalculate pulse width from the Adafruit values
+        w = pulse * (1 / (self.frequency * 4096)) # in seconds
+        w *= 1000 * 1000  # in microseconds
+        w *= 4  # in quarter microsenconds the maestro wants
+        w = int(w)
+
+        with Maestro.maestro_lock:
+            Maestro.maestro_device.write(bytearray([ 0x84,
+                                                     self.channel,
+                                                     (w & 0x7F),
+                                                     ((w >> 7) & 0x7F)]))
+
+    def set_turn_left(self, v):
+        if self.lturn != v:
+            self.lturn = v
+            b = bytearray('L' if v else 'l', 'ascii')
+            with Maestro.astar_lock:
+                Maestro.astar_device.write(b)
+
+    def set_turn_right(self, v):
+        if self.rturn != v:
+            self.rturn = v
+            b = bytearray('R' if v else 'r', 'ascii')
+            with Maestro.astar_lock:
+                Maestro.astar_device.write(b)
+
+    def set_headlight(self, v):
+        if self.headlights != v:
+            self.headlights = v
+            b = bytearray('H' if v else 'h', 'ascii')
+            with Maestro.astar_lock:
+                Maestro.astar_device.write(b)
+
+    def set_brake(self, v):
+        if self.brakelights != v:
+            self.brakelights = v
+            b = bytearray('B' if v else 'b', 'ascii')
+            with Maestro.astar_lock:
+                Maestro.astar_device.write(b)
+
+    def readline(self):
+        ret = None
+        with Maestro.astar_lock:
+            # expecting lines like
+            # E n nnn n
+            if Maestro.astar_device.inWaiting() > 8:
+                ret = Maestro.astar_device.readline()
+
+        if ret != None:
+            ret = ret.rstrip()
+
+        return ret
+
+class Teensy:
+    """
+    Teensy Servo controller
+    """
+    import threading
+
+    teensy_device = None
+    astar_device = None
+    teensy_lock = threading.Lock()
+    astar_lock = threading.Lock()
+
+
+    def __init__(self, channel, frequency = 60):
+        import serial
+
+        if Teensy.teensy_device == None:
+            Teensy.teensy_device = serial.Serial('/dev/teensy', 115200, timeout = 0.01)
+
+        self.channel = channel
+        self.frequency = frequency
+        self.lturn = False
+        self.rturn = False
+        self.headlights = False
+        self.brakelights = False
+
+        if Teensy.astar_device == None:
+            Teensy.astar_device = serial.Serial('/dev/astar', 115200, timeout = 0.01)
+
+    def set_pulse(self, pulse):
+        # Recalculate pulse width from the Adafruit values
+        w = pulse * (1 / (self.frequency * 4096)) # in seconds
+        w *= 1000 * 1000  # in microseconds
+
+        with Teensy.teensy_lock:
+            Teensy.teensy_device.write(("%c %.1f\n" % (self.channel, w)).encode('ascii'))
+
+    def set_turn_left(self, v):
+        if self.lturn != v:
+            self.lturn = v
+            b = bytearray('L' if v else 'l', 'ascii')
+            with Teensy.astar_lock:
+                Teensy.astar_device.write(b)
+
+    def set_turn_right(self, v):
+        if self.rturn != v:
+            self.rturn = v
+            b = bytearray('R' if v else 'r', 'ascii')
+            with Teensy.astar_lock:
+                Teensy.astar_device.write(b)
+
+    def set_headlight(self, v):
+        if self.headlights != v:
+            self.headlights = v
+            b = bytearray('H' if v else 'h', 'ascii')
+            with Teensy.astar_lock:
+                Teensy.astar_device.write(b)
+
+    def set_brake(self, v):
+        if self.brakelights != v:
+            self.brakelights = v
+            b = bytearray('B' if v else 'b', 'ascii')
+            with Teensy.astar_lock:
+                Teensy.astar_device.write(b)
+
+    def teensy_readline(self):
+        ret = None
+        with Teensy.teensy_lock:
+            # expecting lines like
+            # E n nnn n
+            if Teensy.teensy_device.inWaiting() > 8:
+                ret = Teensy.teensy_device.readline()
+
+        if ret != None:
+            ret = ret.rstrip()
+
+        return ret
+
+    def astar_readline(self):
+        ret = None
+        with Teensy.astar_lock:
+            # expecting lines like
+            # E n nnn n
+            if Teensy.astar_device.inWaiting() > 8:
+                ret = Teensy.astar_device.readline()
+
+        if ret != None:
+            ret = ret.rstrip()
+
+        return ret
+
+class MockController(object):
+    def __init__(self):
+        pass
+
+    def run(self, pulse):
+        pass
+
+    def shutdown(self):
+        pass
